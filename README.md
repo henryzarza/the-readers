cfcdb192187c7d5df2b402073a30d121962026d8
Henry Zarza <henryacz7@gmail.com>
2018-01-20 22:06:59 -0500

    Typography settings, color palette and general styles

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # The Readers
 
 This is a personal project using:
-* [node.js] - evented I/O for the backend
-* [Express] - fast node.js network app framework [@tjholowaychuk]
+* [node.js](https://nodejs.org/) - evented I/O for the backend
+* [Express](http://expressjs.com/) - fast node.js network app framework [@tjholowaychuk]
 * [Pug](https://pugjs.org) - Engine view for HTML
 * [MongoDB](https://www.mongodb.com) - Building on the Best of Relational with the Innovations of NoSQL
 
@@ -13,7 +13,7 @@ read by the users. Also it have a chat in realtime using the library socket.io
 
 ### Installation
 
-Dillinger requires [Node.js](https://nodejs.org/) v9+ to run.
+Requires [Node.js](https://nodejs.org/) v9+ to run.
 
 Install the dependencies and devDependencies and start the server.
 
--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const path = require('path')
-// const favicon = require('serve-favicon')
+const favicon = require('serve-favicon')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
@@ -18,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 // uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -26,7 +26,7 @@ app.use(cookieParser())
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
+  indentedSyntax: false, // true = .sass and false = .scss
   sourceMap: true
 }))
 app.use(express.static(path.join(__dirname, 'public')))
