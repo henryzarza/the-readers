9bc599adb37b6e241b9b595029d0384fca148910
Henry Zarza <henryacz7@gmail.com>
2018-01-25 22:00:28 -0500

    Advance in the layout the navbar

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,9 @@ const express = require('express')
 const router = express.Router()
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource')
+router.get('/:username', function (req, res, next) {
+  console.log('Users', req.params)
+  res.render('users', {username: req.params.username})
 })
 
 module.exports = router
