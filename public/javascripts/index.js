30b2fe09c2b8724067728c29a8dca7e606626ebd
Henry Zarza <henryacz7@gmail.com>
2018-01-28 16:01:25 -0500

    Finalization layout cards

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,25 @@
 'use strict'
 
-/* const elContent = document.querySelectorAll('.change__content--login');
-elContent.forEach(el => el.addEventListener('click', (e) => {
-  console.log(e);
-})); */
-
+// Toggle submenu
 const linkSubmenu = document.querySelector('#link--submenu');
 const submenu = document.querySelector('.submenu');
 
 linkSubmenu.addEventListener('click', () => submenu.classList.toggle('submenu--visible'));
+
+// Toggle class active to card
+const addClassActive = document.querySelectorAll('.addClassActive');
+const removeClassActive = document.querySelectorAll('.removeClassActive');
+
+addClassActive.forEach(el => el.addEventListener('click', (e) => {
+    let currentCard = document.querySelector(`.card[data-ref=${e.target.dataset.ref}]`);
+    currentCard.classList.toggle('card--active');
+    setTimeout(() => currentCard.classList.toggle('card--activeEnd'), 300);
+  })
+);
+
+removeClassActive.forEach(el => el.addEventListener('click', (e) => {
+    let currentCard = document.querySelector(`.card[data-ref=${e.target.dataset.ref}]`);
+    currentCard.classList.remove('card--activeEnd');
+    setTimeout(() => currentCard.classList.remove('card--active'), 300);
+  })
+)
