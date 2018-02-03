cfba65450b8c38d2d12ff1a472123b7f3f10a4b3
Henry Zarza <henryacz7@gmail.com>
2018-02-03 14:38:36 -0500

    Layout of change password

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,19 @@
 'use strict'
 
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  console.log('Users')
-  res.render('users')
-})
+  console.log('Users');
+  res.render('users');
+}).get('/changepassword', function (req, res, next) {
+  res.render('changepassword');
+});
 
 /* router.get('/:username', function (req, res, next) {
   console.log('Users', req.params)
   res.render('users', {username: req.params.username})
 })
  */
-module.exports = router
+module.exports = router;
